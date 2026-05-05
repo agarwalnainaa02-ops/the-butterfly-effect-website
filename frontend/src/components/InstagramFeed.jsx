7d522dd594d4feab2bf120080b89bcffa51f5797
Naman Agarwal <naman@tbe.com>
2026-05-05 13:36:23 +0530

    a11y: add aria-label to Instagram post wrapper divs

--- frontend/src/components/InstagramFeed.jsx
+++ frontend/src/components/InstagramFeed.jsx
@@ -64,7 +64,7 @@ const InstagramFeed = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 items-start">
 
         {/* Post 1 */}
-        <div style={{ width: "100%", overflow: "hidden", borderRadius: "3px", aspectRatio: "1 / 1" }}>
+        <div aria-label="Instagram post by The Butterfly Effect" style={{ width: "100%", overflow: "hidden", borderRadius: "3px", aspectRatio: "1 / 1" }}>
           <blockquote
             className="instagram-media"
             data-instgrm-captioned
@@ -86,7 +86,7 @@ const InstagramFeed = () => {
         </div>
 
         {/* Post 2 */}
-        <div style={{ width: "100%", overflow: "hidden", borderRadius: "3px", aspectRatio: "1 / 1" }}>
+        <div aria-label="Instagram post by The Butterfly Effect" style={{ width: "100%", overflow: "hidden", borderRadius: "3px", aspectRatio: "1 / 1" }}>
           <blockquote
             className="instagram-media"
             data-instgrm-captioned
@@ -108,7 +108,7 @@ const InstagramFeed = () => {
         </div>
 
         {/* Post 3 */}
-        <div style={{ width: "100%", overflow: "hidden", borderRadius: "3px", aspectRatio: "1 / 1" }}>
+        <div aria-label="Instagram post by The Butterfly Effect" style={{ width: "100%", overflow: "hidden", borderRadius: "3px", aspectRatio: "1 / 1" }}>
           <blockquote
             className="instagram-media"
             data-instgrm-captioned
